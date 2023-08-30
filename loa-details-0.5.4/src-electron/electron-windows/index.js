3f22ce89fa70076b28f932d0059e1c3d44a7964b
Allen <43168150+allenleekawai@users.noreply.github.com>
2023-08-30 16:05:22 +0800

    Delete loa-details-0.5.4 directory

--- loa-details-0.5.4/src-electron/electron-windows/index.js
+++
@@ -1,5 +0,0 @@
-import { createPrelauncherWindow } from "./prelauncher-window";
-import { createMainWindow } from "./main-window";
-import { createDamageMeterWindow } from "./damage-meter-window";
-
-export { createPrelauncherWindow, createMainWindow, createDamageMeterWindow };
